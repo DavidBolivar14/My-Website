61b32752bc69b01d0dd17474df9e5c9d150709c8
DavidBolivar14 <db21087@students.sacredheart.school.nz>
2024-09-03 23:57:56 +0000

    hbd

--- Myapp.py
+++ Myapp.py
@@ -1,3 +1,15 @@
----
-my first website
----
+
+my first website 
+
+from bottle import run,route,template
+from datetime import datetime
+
+@route('/')
+def index():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return template('homepage', time=current_time)
+
+
+#main routine
+run(reloader=True)
