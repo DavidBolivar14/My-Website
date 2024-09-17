ae8e9245fec8b14f50eebaa8f4f0133209dee2bb
DavidBolivar14 <db21087@students.sacredheart.school.nz>
2024-09-17 23:56:59 +0000

    Latest update

--- Myapp.py
+++ Myapp.py
@@ -1,15 +1,22 @@
-
-my first website 
-
+'''
+My First website with bottle.py
+'''
+import pytz
 from bottle import run,route,template
 from datetime import datetime
+import requests
 
 @route('/')
 def index():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    return template('homepage', time=current_time)
+    now = datetime.now(pytz.timezone('Pacific/Auckland'))
+    current_time = now.strftime("%H:%M:%S:%A2")
+    time = current_time
+    name = 'David'
+    response = requests.get(f"https://api.agify.io/?name={name}")
+    response = response.json()
+    age = response['age']   
+    return template('homepage', time=time, name = name, age=age)
 
 
-#main routine
+#main routine1
 run(reloader=True)
